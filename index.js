375707df9fb80bceb01c70df2325cf26afbbff82
Adam Khodier <khodieradam1297@gmail.com>
2022-09-21 13:31:00 -0400

    commit tree fc6f0720269484547d5a44224a5d9a5b4a3691c9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(Activity="roller-skate") {
+	return `This Saturday, I want to ${Activity}!`
+	}
+
+function mondayWork(Activity="go to the office") {
+	return `This Monday, I will ${Activity}.`
+	}
+
+function wrapAdjective(flair ='*', adj){
+	let wrappedSentence = function (adj = 'special'){
+	return `You are ${flair}${adj}${flair}!`
+	}
+	return wrappedSentence;
+	}
